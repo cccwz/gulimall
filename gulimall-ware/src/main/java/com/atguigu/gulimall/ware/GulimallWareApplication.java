fbe570aeab06148b8bb513a553ceb25aac3c0ab7
cccwz <2365503287@qq.com>
2021-07-29 17:11:13 +0800

    仓库管理-仓库检索

--- gulimall-ware/src/main/java/com/atguigu/gulimall/ware/GulimallWareApplication.java
+++ gulimall-ware/src/main/java/com/atguigu/gulimall/ware/GulimallWareApplication.java
@@ -1,9 +1,15 @@
 package com.atguigu.gulimall.ware;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@MapperScan(basePackages = "com.atguigu.gulimall.ware.dao")
+@EnableTransactionManagement
 public class GulimallWareApplication {
 
     public static void main(String[] args) {
